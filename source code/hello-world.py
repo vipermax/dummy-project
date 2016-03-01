8eba88a314116193bb80a86712acf772acb667bc
Wei Gong <vipermax@gmail.com>
2016-03-01 17:20:26 +0900

    change folder

--- source code/hello-world.py
+++ source code/hello-world.py
@@ -1,7 +1,8 @@
 print "Hello World!"
 print "Hello Again"
 print "I like typing this."
-print "This is fun."
+print "This is a lot of fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+Print "Hey, here is something new!"

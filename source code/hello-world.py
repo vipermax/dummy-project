a2ed1632a1b53edfcc2a38a25d6e518267efbc5c
Wei Gong <vipermax@gmail.com>
2016-03-02 16:18:15 +0900

    bugfix

--- source code/hello-world.py
+++ source code/hello-world.py
@@ -5,6 +5,6 @@ print "This is a lot of fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
-Print "Hey, here is something new!"
+print "Hey, here is something new!"
 print "some udpdate from master branch"
 
